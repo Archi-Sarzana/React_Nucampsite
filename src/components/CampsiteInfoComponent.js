4d318cf26233ce1b7a51f15b3ff9b93173ed127b
SarZie <youremail@domain.com>
2019-12-07 12:21:20 -0500

    week-3 workshop

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -1,24 +1,21 @@
-import React, { Component } from 'react'
-import { Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap';
-class CampsiteInfo extends Component {
-    constructor(props) {
-        super(props)
-        this.state={};
-    }
-    renderCampsite(campsite) {
+import React from 'react'
+import { Card, CardImg, CardText, CardBody, Breadcrumb, BreadcrumbItem } from 'reactstrap';
+import { Link } from 'react-router-dom';
+
+    function RenderCampsite({campsite}) {
         return(
             <div className='col-md-5 m-1'>
                 <Card>
                     <CardImg top src={campsite.image} alt={campsite.name} />
                     <CardBody>
-                        <CardTitle>{campsite.name}</CardTitle>
                         <CardText>{campsite.description}</CardText>
                     </CardBody>
                 </Card>
             </div>
         )
     }
-    renderComments(comments) {
+
+    function RenderComments({comments}) {
         if(comments) {
             return(
                 <div className='col-md-5 m-1'>
@@ -29,33 +26,36 @@ class CampsiteInfo extends Component {
                                 <p>{comment.text}</p>
                                 <p>{new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comment.date)))}</p>
                             </div>
-                        )
+                        );
                     })}
                 </div>
             )
         }
-        return(
-            <div>
-            </div>
-        )
+        return <div />;
     }
-    render() {
-        const {campsite } = this.props
-        if(campsite) {
-            return(
+    
+    function CampsiteInfo(props) {
+        if (props.campsite) {
+            return (
                 <div className="container">
-                    <div className='row'>
-                        {this.renderCampsite(campsite)}
-                    {this.renderComments(campsite.comments)}
+                    <div className="row">
+                        <div className="col">
+                            <Breadcrumb>
+                                <BreadcrumbItem><Link to="/directory">Directory</Link></BreadcrumbItem>
+                                <BreadcrumbItem active>{props.campsite.name}</BreadcrumbItem>
+                            </Breadcrumb>
+                            <h2>{props.campsite.name}</h2>
+                            <hr />
+                        </div>
+                    </div>
+                    <div className="row">
+                        <RenderCampsite campsite={props.campsite} />
+                        <RenderComments comments={props.comments} />
                     </div>
                 </div>
-            )
-        } else {
-            return(
-                <div>
-                </div>
-            )
+            );
         }
+        return <div />;
     }
-}
-export default CampsiteInfo;
+    
+    export default CampsiteInfo;
--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,33 +1,63 @@
 import React, { Component } from 'react';
-import { Navbar, NavbarBrand } from 'reactstrap';
 import Directory from './/DirectoryComponents';
 import CampsiteInfo from './CampsiteInfoComponent';
+import Header from './HeaderComponent';
+import Footer from './FooterComponent';
+import Home from './HomeComponent';
+import Contact from './ContactComponent';
+import AboutComponent from './AboutComponent';
+import { Switch, Route, Redirect } from 'react-router-dom';
 import { CAMPSITES } from '../Shared/campsites';
+import { COMMENTS } from '../Shared/comments';
+import { PARTNERS } from '../Shared/partners';
+import { PROMOTIONS } from '../Shared/promotions';
 
 
 class Main extends Component {
     constructor(props) {
-      super(props);
-      this.state = {
-        campsites: CAMPSITES,
-        selectedCampsite: null
+        super(props);
+        this.state = {
+          campsites: CAMPSITES,
+          comments: COMMENTS,
+          partners: PARTNERS,
+          promotions: PROMOTIONS
       };
     }
 
-    onCampsiteSelect(campsiteId) {
-        this.setState({selectedCampsite: campsiteId});
-    }
-
     render() {
+
+      const HomePage = () => {
+          return (
+            <Home
+              campsite={this.state.campsites.filter(campsite => campsite.featured)[0]}
+              promotion={this.state.promotions.filter(promotion => promotion.featured)[0]}
+              partner={this.state.partners.filter(partner => partner.featured)[0]}
+            />
+          );
+      }
+
+        const CampsiteWithId = ({match}) => {
+          return (
+              <CampsiteInfo 
+                  campsite={this.state.campsites.filter(campsite => campsite.id === +match.params.campsiteId)[0]}
+                  comments={this.state.comments.filter(comment => comment.campsiteId === +match.params.campsiteId)}
+              />
+          );
+      }; 
+
+
       return (
         <div>
-          <Navbar dark color="primary">
-          <div className="container">
-            <NavbarBrand href="/">NuCamp</NavbarBrand>
-          </div>
-          </Navbar>
-          <Directory campsites={this.state.campsites} onClick={campsiteId => this.onCampsiteSelect(campsiteId)}/>
-          <CampsiteInfo campsite={this.state.campsites.filter(campsite => campsite.id === this.state.selectedCampsite)[0]} />
+          <Header />
+          <Switch>
+              <Route path='/home' component={HomePage} />
+              <Route exact path='/directory' render={() => <Directory campsites={this.state.campsites} />} />
+              <Route path='/directory/:campsiteId' component={CampsiteWithId} />
+              <Route exact path='/contactus' component={Contact} />
+              <Route path='/about' render={() => <AboutComponent partners={this.state.partners}/>}/>
+              <Redirect to='/home' />
+          </Switch>
+          <Footer />
         </div>
       );
     }
